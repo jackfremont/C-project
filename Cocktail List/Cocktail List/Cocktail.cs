93238e8abd096af8545b7f44ddf69a4d9de356d5
jackfremont <51928301+jackfremont@users.noreply.github.com>
2019-06-18 19:17:44 -0700

    Add files via upload

---
+++ Cocktail List/Cocktail List/Cocktail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cocktail_List
+{
+
+
+    class Cocktail
+    {
+        private List<Cocktail> Cocktails = new List<Cocktail>();
+
+
+        #region Properties
+
+
+        public string Picturelink { get; set; }
+
+        public string Name { get; set; }
+
+        public Array IngredientList { get; set; }
+
+        public DateTime InputDate { get; private set; }
+
+        public int Rating { get; set; }
+        #endregion
+
+    }
+
+    
+}
---
+++ Cocktail List/Cocktail List/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cocktail_List
+{
+    class Ingredient
+    {
+        public string IngredientName { get; set; }
+
+        public decimal amount { get; set; }
+    }
+}
